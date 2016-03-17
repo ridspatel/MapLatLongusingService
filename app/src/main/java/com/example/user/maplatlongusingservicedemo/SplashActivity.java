f8ac9df43f97d2a6fd9c8ca3cec009a3a0d45d65
ridspatel <patelriddhi1993@gmail.com>
2016-03-17 11:07:41 +0530

    Riddhi

--- app/src/main/java/com/example/user/maplatlongusingservicedemo/SplashActivity.java
+++ app/src/main/java/com/example/user/maplatlongusingservicedemo/SplashActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class SplashActivity extends Activity {
-    //Riddhi Patel
+
     private Button btnSingleServiceClass, btnServiceManager;
 
     @Override
